2fcce552199297d928ea0666041c6e23820f35f1
Camden Cheek <ccheek22@gmail.com>
2018-12-27 16:47:27 -0500

    Fix rubocop offenses

--- config/software/rubygems.rb
+++ config/software/rubygems.rb
@@ -14,13 +14,13 @@
 # limitations under the License.
 #
 
-name "rubygems"
+name 'rubygems'
 
-license "MIT"
-license_file "https://raw.githubusercontent.com/rubygems/rubygems/master/LICENSE.txt"
+license 'MIT'
+license_file 'https://raw.githubusercontent.com/rubygems/rubygems/master/LICENSE.txt'
 skip_transitive_dependency_licensing true
 
-dependency "ruby"
+dependency 'ruby'
 
 if version && !source
   # NOTE: 2.1.11 is the last version of rubygems before the 2.2.x change to native gem install location
@@ -31,16 +31,16 @@ if version && !source
   # We have switched from tarballs to just `gem update --system`, but for backcompat
   # we pin the previously known tarballs.
   known_tarballs = {
-    "2.1.11" => "b561b7aaa70d387e230688066e46e448",
-    "2.2.1" => "1f0017af0ad3d3ed52665132f80e7443",
-    "2.4.1" => "7e39c31806bbf9268296d03bd97ce718",
-    "2.4.4" => "440a89ad6a3b1b7a69b034233cc4658e",
-    "2.4.5" => "5918319a439c33ac75fbbad7fd60749d",
-    "2.4.8" => "dc77b51449dffe5b31776bff826bf559",
-    "2.6.7" => "9cd4c5bdc70b525dfacd96e471a64605",
-    "2.6.8" => "40b3250f28c1d0d5cb9ff5ab2b17df6e",
-    "2.6.13" => "e6f19b51614055d826e431549a12a80b",
-    "2.6.14" => "e0a6914ce03b91dfc6d448ebf292f145",
+    '2.1.11' => 'b561b7aaa70d387e230688066e46e448',
+    '2.2.1' => '1f0017af0ad3d3ed52665132f80e7443',
+    '2.4.1' => '7e39c31806bbf9268296d03bd97ce718',
+    '2.4.4' => '440a89ad6a3b1b7a69b034233cc4658e',
+    '2.4.5' => '5918319a439c33ac75fbbad7fd60749d',
+    '2.4.8' => 'dc77b51449dffe5b31776bff826bf559',
+    '2.6.7' => '9cd4c5bdc70b525dfacd96e471a64605',
+    '2.6.8' => '40b3250f28c1d0d5cb9ff5ab2b17df6e',
+    '2.6.13' => 'e6f19b51614055d826e431549a12a80b',
+    '2.6.14' => 'e0a6914ce03b91dfc6d448ebf292f145',
   }
   known_tarballs.each do |vsn, md5|
     version vsn do
@@ -49,12 +49,12 @@ if version && !source
     end
   end
 
-  version("v2.4.4_plus_debug") { source git: "https://github.com/danielsdeleo/rubygems.git" }
-  version("2.4.4.debug.1")     { source git: "https://github.com/danielsdeleo/rubygems.git" }
+  version('v2.4.4_plus_debug') { source git: 'https://github.com/danielsdeleo/rubygems.git' }
+  version('2.4.4.debug.1')     { source git: 'https://github.com/danielsdeleo/rubygems.git' }
   # This is the 2.4.8 release with a fix for
   # windows so things like `gem install "pry"` still
   # work
-  version("jdm/2.4.8-patched") { source git: "https://github.com/jaym/rubygems.git" }
+  version('jdm/2.4.8-patched') { source git: 'https://github.com/jaym/rubygems.git' }
 end
 
 # If we still don't have a source (if it's a tarball) grab from ruby ...
@@ -64,13 +64,13 @@ if version && !source
   begin
     Gem::Version.new(version)
   rescue ArgumentError
-    source git: "https://github.com/rubygems/rubygems.git"
+    source git: 'https://github.com/rubygems/rubygems.git'
   end
 end
 
 # git repo is always expanded to "rubygems"
 if source && source.include?(:git)
-  relative_path "rubygems"
+  relative_path 'rubygems'
 end
 
 build do
@@ -78,7 +78,7 @@ build do
 
   if source
     # Building from source:
-    ruby "setup.rb --no-ri --no-rdoc", env: env
+    ruby 'setup.rb --no-ri --no-rdoc', env: env
   else
     # Installing direct from rubygems:
     # If there is no version, this will get latest.
