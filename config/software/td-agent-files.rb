e76151c0201c03f17fa66fa94bc3646a245d0cca
Camden Cheek <ccheek22@gmail.com>
2018-12-27 16:48:08 -0500

    Refactor to remove arbitrary code blocks

--- config/software/td-agent-files.rb
+++ config/software/td-agent-files.rb
@@ -6,93 +6,128 @@ dependency "td-agent"
 # This software setup td-agent related files, e.g. etc files.
 # Separating file into td-agent.rb and td-agent-files.rb is for speed up package building
 
+
 build do
-  block do
-    # setup related files
-    pkg_type = project.packagers_for_system.first.id.to_s
-    root_path = "/" # for ERB
-    install_path = project.install_dir # for ERB
-    project_name = project.name # for ERB
-    is_td_agent2 = project.build_version.start_with?('2.')
-    project_name_snake = project.name.gsub('-', '_') # for variable names in ERB
-    rb_major, rb_minor, rb_teeny = project.overrides[:ruby][:version].split("-", 2).first.split(".", 3)
-    gem_dir_version = "#{rb_major}.#{rb_minor}.0" # gem path's teeny version is always 0
-    install_message = nil
+  pkg_type = project.packagers_for_system.first.id.to_s
+  root_path = "/" # for ERB
+  install_path = project.install_dir # for ERB
+  project_name = project.name # for ERB
+  rb_major, rb_minor, rb_teeny = project.overrides[:ruby][:version].split("-", 2).first.split(".", 3)
+  gem_dir_version = "#{rb_major}.#{rb_minor}.0" # gem path's teeny version is always 0
+  install_message = "Install successful"
+  commander_name = "fluent-commander"
 
-    template = ->(*parts) { File.join('templates', *parts) }
-    generate_from_template = ->(dst, src, erb_binding, opts={}) {
-      mode = opts.fetch(:mode, 0755)
-      destination = dst.gsub('td-agent', project.name)
-      FileUtils.mkdir_p File.dirname(destination)
-      File.open(destination, 'w', mode) do |f|
-        f.write ERB.new(File.read(src), nil, '<>').result(erb_binding)
-      end
-    }
-
-    if File.exist?(template.call('INSTALL_MESSAGE'))
-      install_message = File.read(template.call('INSTALL_MESSAGE'))
+  generate_from_template = ->(dst, src, erb_binding, opts={}) {
+    mode = opts.fetch(:mode, 0755)
+    destination = dst.gsub('td-agent', project.name)
+    FileUtils.mkdir_p File.dirname(destination)
+    File.open(destination, 'w', mode) do |f|
+      f.write ERB.new(File.read(src), nil, '<>').result(erb_binding)
     end
+  }
 
-    # copy pre/post scripts into omnibus path (./package-scripts/td-agentN)
-    FileUtils.mkdir_p(project.package_scripts_path)
-    Dir.glob(File.join(project.package_scripts_path, '*')).each { |f|
-      FileUtils.rm_f(f) if File.file?(f)
-    }
-    # templates/package-scripts/td-agent/xxxx/* -> ./package-scripts/td-agentN
-    Dir.glob(template.call('package-scripts', 'td-agent', pkg_type, '*')).each { |f|
-      package_script = File.join(project.package_scripts_path, File.basename(f))
-      generate_from_template.call package_script, f, binding, mode: 0755
-    }
 
-    # setup plist / init.d file
-    if ['pkg', 'dmg'].include?(pkg_type)
-      # templates/td-agent.plist.erb -> INSTALL_PATH/td-agent.plist
-      plist_path = File.join(install_path, "td-agent.plist")
-      generate_from_template.call plist_path, template.call("td-agent.plist.erb"), binding, mode: 0755
-    else
-      # templates/etc/init.d/xxxx/td-agent -> ./resources/etc/init.d/td-agent
-      initd_file_path = File.join(project.resources_path, 'etc', 'init.d', project.name)
-      template_path = template.call('etc', 'init.d', pkg_type, 'td-agent')
-      if File.exist?(template_path)
-        generate_from_template.call initd_file_path, template_path, binding, mode: 0755
-      end
+  if File.exist?(File.join('INSTALL_MESSAGE'))
+    install_message = File.read(File.join('INSTALL_MESSAGE'))
+  end
+  def sub_name(file)
+    file.gsub('td-agent', project.name)
+  end
 
-      # render fluentd service file
-      systemd_file_path = File.join(project.resources_path, 'etc', 'systemd', "#{project_name}.service")
-      template_path = template.call('etc', 'systemd', 'td-agent.service.erb')
-      if File.exist?(template_path)
-        generate_from_template.call systemd_file_path, template_path, binding, mode: 0755
-      end
+  delete "#{project.package_scripts_path}/*"
+  Dir.glob(File.join('package-scripts', 'td-agent', pkg_type, '*')).each do |src|
+    dst = File.join(project.package_scripts_path, File.basename(src))
+    mkdir File.dirname(dst)
+    erb source: src,
+        dest: sub_name(dst),
+        mode: 0o755,
+        vars: {
+            project_name: project_name,
+            install_message: install_message,
+            gem_dir_version: gem_dir_version
+        }
+  end
 
-      # render commander service file
-      systemd_file_path = File.join(project.resources_path, 'etc', 'systemd', "fluent-commander.service")
-      template_path = template.call('etc', 'systemd', 'fluent-commander.service.erb')
-      if File.exist?(template_path)
-        generate_from_template.call systemd_file_path, template_path, binding, mode: 0755
-      end
-    end
+  if %w[pkg dmg].include?(pkg_type)
+    plist_path = File.join(install_path, "td-agent.plist")
+    mkdir File.dirname(plist_path)
+    erb source: "td-agent.plist.erb",
+        dest: sub_name(plist_path),
+        mode: 0o755,
+        vars: {
+          project_name: project_name,
+          install_path: install_path,
+          gem_dir_version: gem_dir_version
+        }
+  else
+    initd_file_path = File.join(project.resources_path, 'etc', 'init.d', project.name)
+    template_path = File.join('etc', 'init.d', pkg_type, 'td-agent')
+    mkdir File.dirname(initd_file_path)
+    erb source: template_path,
+        dest: sub_name(initd_file_path),
+        mode: 0o755,
+        vars: {
+          project_name: project_name,
+          install_path: install_path,
+          gem_dir_version: gem_dir_version
+        }
 
-    # setup /etc/td-agent
-    ['td-agent.conf', 'td-agent.conf.tmpl', ['logrotate.d', 'td-agent.logrotate'], ['prelink.conf.d', 'td-agent.conf']].each { |item|
+    systemd_file_path = File.join(project.resources_path, 'etc', 'systemd', "#{project_name}.service")
+    template_path = File.join('etc', 'systemd', 'td-agent.service.erb')
+    mkdir File.dirname(systemd_file_path)
+    erb source: template_path,
+        dest: sub_name(systemd_file_path),
+        mode: 0o755,
+        vars: {
+          project_name: project_name,
+          install_path: install_path,
+          gem_dir_version: gem_dir_version
+        }
+
+    # render commander service file
+    systemd_file_path = File.join(project.resources_path, 'etc', 'systemd', "#{commander_name}.service")
+    template_path = File.join('etc', 'systemd', "#{commander_name}.service.erb")
+    mkdir File.dirname(systemd_file_path)
+    erb source: template_path,
+        dest: sub_name(systemd_file_path),
+        mode: 0o755,
+        vars: {
+          project_name: project_name,
+          install_path: install_path,
+          gem_dir_version: gem_dir_version,
+          commander_name: commander_name
+        }
+
+    ['td-agent.conf', 'td-agent.conf.tmpl', %w[logrotate.d td-agent.logrotate], %w(prelink.conf.d td-agent.conf)].each do |item|
       conf_path = File.join(project.resources_path, 'etc', project_name, *([item].flatten))
-      generate_from_template.call conf_path, template.call('etc', 'td-agent', *([item].flatten)), binding, mode: 0644
-    }
-
-    ["td-agent", "td-agent-gem"].each { |command|
-      sbin_path = File.join(install_path, 'usr', 'sbin', command)
-      # templates/usr/sbin/yyyy.erb -> INSTALL_PATH/usr/sbin/yyyy
-      generate_from_template.call sbin_path, template.call('usr', 'sbin', "#{command}.erb"), binding, mode: 0755
-    }
-
-    FileUtils.remove_entry_secure(File.join(install_path, 'etc'), true)
-    # ./resources/etc -> INSTALL_PATH/etc
-    FileUtils.cp_r(File.join(project.resources_path, 'etc'), install_path)
-
-    # Windows Shortcut
-    if windows?
-      f = File.join(install_path, 'td-agent-prompt.bat')
-      FileUtils.rm_f(f) if File.file?(f)
-      FileUtils.cp(File.join(project.resources_path, 'msi', 'td-agent-prompt.bat'), f)
+      template_path = File.join('etc', 'td-agent', *([item].flatten))
+      mkdir File.dirname(conf_path)
+      erb source: template_path,
+          dest: sub_name(conf_path),
+          mode: 0o644,
+          vars: {
+            project_name: project_name,
+            install_path: install_path,
+            gem_dir_version: gem_dir_version
+          }
     end
+
+
+    %w[td-agent td-agent-gem].each do |command|
+      sbin_path = File.join(install_path, 'usr', 'sbin', command)
+      template_path = File.join('usr', 'sbin', "#{command}.erb")
+      mkdir File.dirname(sbin_path)
+      erb source: template_path,
+          dest: sub_name(sbin_path),
+          mode: 0o644,
+          vars: {
+              project_name: project_name,
+              install_path: install_path,
+              gem_dir_version: gem_dir_version
+          }
+    end
+
+    delete File.join(install_path, 'etc')
+    copy File.join(project.resources_path, 'etc'), install_path
   end
 end
