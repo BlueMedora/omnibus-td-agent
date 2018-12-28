e0acdaccca79afa24791fa25c9cd1bdea03fcccb
Camden Cheek <ccheek22@gmail.com>
2018-12-28 16:42:01 -0500

    Use environment for github token

--- config/software/td-agent.rb
+++ config/software/td-agent.rb
@@ -10,5 +10,5 @@ dependency 'fluentd'
 build do
   env = with_standard_compiler_flags(with_embedded_path)
   gemfile = File.join(Omnibus::Config.project_root, 'InstalledGemfile')
-  bundle "install --gemfile=#{gemfile}", :env => { 'GITHUB_TOKEN' => '64783e90a7deaaa24a9fd6763635a485780c6590' }
+  bundle "install --gemfile=#{gemfile}", :env => ENV
 end
