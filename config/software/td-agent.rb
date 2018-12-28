59a8630419149a3cb682511c7721380022eb6e22
Camden Cheek <ccheek22@gmail.com>
2018-12-28 16:13:38 -0500

    Use gemfile rather than raw gems

--- config/software/td-agent.rb
+++ config/software/td-agent.rb
@@ -9,13 +9,6 @@ dependency 'fluentd'
 
 build do
   env = with_standard_compiler_flags(with_embedded_path)
-
-  Dir.glob(File.expand_path(File.join(Omnibus::Config.project_root, 'plugin_gems', '*.gem'))).sort.each { |gem_path|
-    args = ''
-    if project.ohai['platform_family'] == 'mac_os_x' && gem_path.include?('-thrift-')
-      # See: https://issues.apache.org/jira/browse/THRIFT-2219
-      args << " -- --with-cppflags='-D_FORTIFY_SOURCE=0'"
-    end
-    gem "install --no-ri --no-rdoc #{gem_path} #{args}", :env => env
-  }
+  gemfile = File.join(Omnibus::Config.project_root, 'InstalledGemfile')
+  bundle "install --gemfile=#{gemfile}"
 end
