7fb2de566eb7bd40778efdc2beea7a8daae0f11b
Camden Cheek <ccheek22@gmail.com>
2019-01-03 10:16:08 -0500

    Use files path as a point of reference

--- config/software/td-agent.rb
+++ config/software/td-agent.rb
@@ -9,6 +9,5 @@ dependency 'fluentd'
 
 build do
   env = with_standard_compiler_flags(with_embedded_path)
-  bundle "install --gemfile=#{File.join(project_root, 'PluginGemfile')}", env: ENV
-  
+  bundle "install --gemfile=#{File.join(project.files_path, 'PluginGemfile')}", env: ENV
 end
