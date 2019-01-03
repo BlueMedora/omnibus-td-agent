3253d4ed50c22a06cd5912d2b5132a7ba815aea3
Camden Cheek <ccheek22@gmail.com>
2019-01-03 11:21:07 -0500

    Fix typo

--- config/software/td-agent.rb
+++ config/software/td-agent.rb
@@ -10,9 +10,9 @@ dependency 'fluentd'
 build do
   env = with_standard_compiler_flags(with_embedded_path)
 
-  gem "build #{ENV['FLUENTD_COMMANDER_SOURCE']}/fluentd_agent.gemspec", :cws => ENV['FLUENTD_COMMANDER_SOURCE']
-  gem "install --local #{ENV['FLUENTD_COMMANDER_SOURCE']}/fluentd_agent*.gem", :cws => ENV['FLUENTD_COMMANDER_SOURCE']
+  gem "build #{ENV['FLUENTD_COMMANDER_SOURCE']}/fluentd_agent.gemspec", :cwd => ENV['FLUENTD_COMMANDER_SOURCE']
+  gem "install --local #{ENV['FLUENTD_COMMANDER_SOURCE']}/fluentd_agent*.gem", :cwd => ENV['FLUENTD_COMMANDER_SOURCE']
 
-  gem "build #{ENV['FLUENTD_STACKDRIVER_SOURCE']}/fluentd-stackdriver-plugin.gemspec", :cws => ENV['FLUENTD_STACKDRIVER_SOURCE']
-  gem "install --local #{ENV['FLUENTD_STACKDRIVER_SOURCE']}/fluentd-stackdriver-plugin*.gem", :cws => ENV['FLUENTD_STACKDRIVER_SOURCE']
+  gem "build #{ENV['FLUENTD_STACKDRIVER_SOURCE']}/fluentd-stackdriver-plugin.gemspec", :cwd => ENV['FLUENTD_STACKDRIVER_SOURCE']
+  gem "install --local #{ENV['FLUENTD_STACKDRIVER_SOURCE']}/fluentd-stackdriver-plugin*.gem", :cwd => ENV['FLUENTD_STACKDRIVER_SOURCE']
 end
