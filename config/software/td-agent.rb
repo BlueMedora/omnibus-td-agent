eeecf82802c309ad669614b6ac92caa919f78211
Camden Cheek <ccheek22@gmail.com>
2019-01-03 11:22:54 -0500

    Remove local requirement

--- config/software/td-agent.rb
+++ config/software/td-agent.rb
@@ -11,8 +11,8 @@ build do
   env = with_standard_compiler_flags(with_embedded_path)
 
   gem "build #{ENV['FLUENTD_COMMANDER_SOURCE']}/fluentd_agent.gemspec", :cwd => ENV['FLUENTD_COMMANDER_SOURCE']
-  gem "install --local #{ENV['FLUENTD_COMMANDER_SOURCE']}/fluentd_agent*.gem", :cwd => ENV['FLUENTD_COMMANDER_SOURCE']
+  gem "install #{ENV['FLUENTD_COMMANDER_SOURCE']}/fluentd_agent*.gem", :cwd => ENV['FLUENTD_COMMANDER_SOURCE']
 
   gem "build #{ENV['FLUENTD_STACKDRIVER_SOURCE']}/fluentd-stackdriver-plugin.gemspec", :cwd => ENV['FLUENTD_STACKDRIVER_SOURCE']
-  gem "install --local #{ENV['FLUENTD_STACKDRIVER_SOURCE']}/fluentd-stackdriver-plugin*.gem", :cwd => ENV['FLUENTD_STACKDRIVER_SOURCE']
+  gem "install #{ENV['FLUENTD_STACKDRIVER_SOURCE']}/fluentd-stackdriver-plugin*.gem", :cwd => ENV['FLUENTD_STACKDRIVER_SOURCE']
 end
