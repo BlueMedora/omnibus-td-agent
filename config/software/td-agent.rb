05fab9c08a2b9c01b1b4e864eab33c8177fa75d2
Camden Cheek <ccheek22@gmail.com>
2019-01-03 10:47:56 -0500

    Fixed variable interpolation

--- config/software/td-agent.rb
+++ config/software/td-agent.rb
@@ -10,9 +10,9 @@ dependency 'fluentd'
 build do
   env = with_standard_compiler_flags(with_embedded_path)
 
-  gem "build #{ENV['FLUENTD_COMMANDER_SOURCE']/fluentd_agent.gemspec}"
+  gem "build #{ENV['FLUENTD_COMMANDER_SOURCE']}/fluentd_agent.gemspec"
   gem "install fluentd_agent", :cwd => ENV['FLUENTD_COMMANDER_SOURCE']
 
-  gem "build #{ENV['FLUENTD_STACKDRIVER_SOURCE']/fluentd-stackdriver-plugin.gemspec}"
+  gem "build #{ENV['FLUENTD_STACKDRIVER_SOURCE']}/fluentd-stackdriver-plugin.gemspec"
   gem "install fluentd-stackdriver-plugin", :cwd => ENV['FLUENTD_STACKDRIVER_SOURCE']
 end
